51316549f854469d877d563f3bd5ad7dffa21826
Red Daly <reddaly@gmail.com>
2013-09-06 00:15:17 -0700

    Fix bug in NodeJS code generator.
    The code generator was unintentionally assigning the _seqid property
    of connection clients to undefined.

--- compiler/cpp/src/generate/t_js_generator.cc
+++ compiler/cpp/src/generate/t_js_generator.cc
@@ -1010,7 +1010,7 @@ void t_js_generator::generate_service_client(t_service* tservice) {
   // utils for multiplexed services
   if (gen_node_) {
     indent(f_service_) <<  js_namespace(tservice->get_program())<<service_name_ << "Client.prototype.seqid = function() { return this._seqid; }" << endl <<
-    js_namespace(tservice->get_program())<<service_name_ << "Client.prototype.new_seqid = function() { this._seqid += 1; }" << endl;
+    js_namespace(tservice->get_program())<<service_name_ << "Client.prototype.increment_seqid = function() { this._seqid++; }" << endl;
   }
   // Generate client method implementations
   vector<t_function*> functions = tservice->get_functions();
@@ -1030,7 +1030,7 @@ void t_js_generator::generate_service_client(t_service* tservice) {
 
     if (gen_node_) {
       f_service_ <<
-        indent() << "this._seqid = this.new_seqid();" << endl <<
+        indent() << "this.increment_seqid();" << endl <<
         indent() << "this._reqs[this.seqid()] = callback;" << endl;
     } else if (gen_jquery_) {
       f_service_ <<
